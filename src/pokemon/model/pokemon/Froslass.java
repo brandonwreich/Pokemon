d45e8f1fd07cbd9ce64de42b0e8a68aef6cf3a6a
Brandon Reich <31513789+brandonwreich@users.noreply.github.com>
2017-12-18 08:09:32 -0700

    created my Froslass pokemon

---
+++ src/pokemon/model/pokemon/Froslass.java
@@ -0,0 +1,70 @@
+package pokemon.model.pokemon;
+
+import pokemon.model.Pokemon;
+import pokemon.model.types.Ghost;
+import pokemon.model.types.Ice;
+
+public class Froslass extends Pokemon implements Ghost, Ice
+{
+	public Froslass(int number, String name)
+	{
+		super(number, name);
+	}
+	
+	public int scare(String booAmount)
+	{
+		int damage = 1;
+		
+		if(booAmount.length() > 5)
+		{
+			damage = 1200;
+		}
+		
+		else
+		{
+			damage = 2;
+		}
+		
+		return damage;
+	}
+	
+	public boolean disappear()
+	{
+		boolean didDisappear = false;
+		
+		return didDisappear;
+	}
+	
+	public boolean possess()
+	{
+		boolean didPossess = false;
+		
+		return didPossess;
+	}
+	
+	public Beartic(int number, String name)
+	{
+		super(number, name);
+	}
+	
+	public int freeze()
+	{
+		int cold = -45;
+		
+		return cold;
+	}
+	
+	public int frostBite()
+	{
+		int bite = -854326;
+		
+		return bite;
+	}
+	
+	public int shield()
+	{
+		int iceShield = 454646;
+		
+		return iceShield;
+	}
+}
