b777462549a3b2263ce490f06b7a89288b8a2e23
Brandon Reich <31513789+brandonwreich@users.noreply.github.com>
2017-12-18 09:34:35 -0700

    designing my panel

--- src/pokemon/view/PokemonPanel.java
+++ src/pokemon/view/PokemonPanel.java
@@ -2,6 +2,7 @@ package pokemon.view;
 
 import pokemon.controller.PokemonController;
 import javax.swing.JPanel;
+import javax.swing.*;
 
 /**
  * 
@@ -13,10 +14,46 @@ public class PokemonPanel extends JPanel
 {
 	private PokemonController appController;
 	
+	private JLabel pictureLabel;
+	private JLabel numberLabel;
+	private JLabel nameLabel;
+	private JLabel evolveLabel;
+	private JLabel healthLabel;
+	private JLabel attackLabel;
+	
+	private JTextArea numberArea;
+	private JTextArea nameArea;
+	private JTextArea healthArea;
+	private JTextArea attackArea;
+	private JTextArea textArea;
+	private JTextArea textArea2;
+	
+	private JButton loadButton;
+	private JButton saveButton;
+	private JButton resetButton;
+	
+	private JCheckBox canEvolveBox;
+	
+	private JComboBox comboBox;
+	
+	private SpringLayout appLayout;
+	
+	
 	public PokemonPanel(PokemonController appController)
 	{
 		super();
 		this.appController = appController;
+		appLayout = new SpringLayout();
+		
+		pictureLabel = new JLabel("");
+		numberLabel = new JLabel("Number");
+		evolveLabel = new JLabel("Can Evolve");
+		healthLabel = new JLabel("Health");
+		attackLabel = new JLabel("Attack");
+		
+		numberArea = new JTextArea(5, 5);
+		nameArea = new JTextArea(5, 5);
+		
 		
 		setupPanel();
 		setupLayout();
