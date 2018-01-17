c4238fed744341cc30f752346a94f3ce2e4853a0
Brandon Reich <31513789+brandonwreich@users.noreply.github.com>
2018-01-17 09:06:28 -0700

    Created a file class to save my files

---
+++ src/pokemon/controller/FileController.java
@@ -0,0 +1,40 @@
+package pokemon.controller;
+
+import pokemon.model.*;
+import java.util.*;
+import java.io.*;
+import pokemon.view.*;
+
+public class FileController
+{
+	private static PopupDisplay popup = new PopupDisplay();
+
+	public static void savePokemonToFile(ArrayList<Pokemon> pokedex)
+	{
+		File saveFile = new File("Saved Pokedex.txt.");
+		try
+		{
+			PrintWriter writeToDisk = new PrintWriter(saveFile);
+
+			for (int index = 0; index < pokedex.size(); index++)
+			{
+				String name = pokedex.get(index).getName();
+				int number = pokedex.get(index).getNumber();
+				int attack = pokedex.get(index).getAttackPoints();
+				int health = pokedex.get(index).getHealthPoints();
+				boolean evolve = pokedex.get(index).isCanEvolve();
+				double modify = pokedex.get(index).getEnhancementModifier();
+
+				String row = name + "," + number + "," + health + "," + attack + "," + modify + "," + evolve;
+
+				writeToDisk.println(row);
+			}
+
+			writeToDisk.close();
+		}
+		catch (FileNotFoundException error)
+		{
+			popup.displayText("There was an error: " + error.getMessage());
+		}
+	}
+}
